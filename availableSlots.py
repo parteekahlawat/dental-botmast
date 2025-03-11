3f18432d5abefb1e0040c367fce9cae80204076b
parteekahlawat <prateekahlawat1223@gmail.com>
2025-03-11 12:43:48 +0530

    pagination added

--- availableSlots.py
+++ availableSlots.py
@@ -14,45 +14,50 @@ def get_slots(timeNow: int, timeStart: int, timeEnd: int, reasonId: str, userTyp
     slot_list = []
     pageNo = 1
     while(page):
-        url = f"https://uk.mydentalhub.online/v31/events?timestamp={timeNow}&startDay={timeStart}&endDay={timeEnd}&patientId=0fa43773-b8bd-4b1a-8c5a-2f0580f03673&payorType=Private&patientType={userType}&reasonId={reasonId}&practiceId=UKSHQ02&firstEventForProvider=false{providerLink}"
         try:
+            url = f"https://uk.mydentalhub.online/v31/events?timestamp={timeNow}&startDay={timeStart}&endDay={timeEnd}&patientId=0fa43773-b8bd-4b1a-8c5a-2f0580f03673&payorType=Private&patientType={userType}&reasonId={reasonId}&practiceId=UKSHQ02&firstEventForProvider=false{providerLink}"
             response = requests.get(url)
             if response.status_code == 200:
                 data = response.json()
-                # weekData = data[:7]
-                # return jsonify(weekData)
-                page_list = []
-                for event in data:
-                    name = ''
-                    price = ''
-                    deposit = ''
-                    starting = event.get("startTime")
-                    duration = event.get("duration")
-                    starting = datetime.datetime.fromisoformat(starting)
-                    date = starting.astimezone(uk_timezone).strftime("%d")
-                    month = starting.astimezone(uk_timezone).strftime("%B")
-                    year = starting.astimezone(uk_timezone).strftime("%Y")
-                    time = starting.astimezone(uk_timezone).strftime("%I:%M %p")
-                    
-                    uk_end_time = starting.astimezone(uk_timezone) + datetime.timedelta(minutes=duration)
-                    end_time = uk_end_time.strftime("%I:%M %p")
-                    for i in event.get("resourceEvents", []):
-                        name = i.get("resourceName")
-                        price = i.get("salesInformation", {}).get("price", {}).get("amount")
-                        deposit = i.get("salesInformation", {}).get("deposit", {}).get("amount")
-                    page_list.append({
-                        "Id": event.get('id'),
-                        "Name": name,
-                        "Start Time": f"{date} {month} {year}, {time}",
-                        "End Time": f"{date} {month} {year}, {end_time}",
-                        "Duration": duration,
-                        "Price": price,
-                        "Deposit Amount": deposit,
+                if len(data)==0:
+                    timeStart = timeEnd
+                    end_of_day = datetime.datetime.fromtimestamp(timeEnd / 1000)  + datetime.timedelta(days=7)
+                    end_of_day_midnight = end_of_day.replace(hour=23, minute=59, second=59, microsecond=999999)
+                    timeEnd = int(end_of_day_midnight.timestamp() * 1000)
+                    continue
+                if page==1:
+                    page_list = []
+                    for event in data:
+                        name = ''
+                        price = ''
+                        deposit = ''
+                        starting = event.get("startTime")
+                        duration = event.get("duration")
+                        starting = datetime.datetime.fromisoformat(starting)
+                        date = starting.astimezone(uk_timezone).strftime("%d")
+                        month = starting.astimezone(uk_timezone).strftime("%B")
+                        year = starting.astimezone(uk_timezone).strftime("%Y")
+                        time = starting.astimezone(uk_timezone).strftime("%I:%M %p")
+
+                        uk_end_time = starting.astimezone(uk_timezone) + datetime.timedelta(minutes=duration)
+                        end_time = uk_end_time.strftime("%I:%M %p")
+                        for i in event.get("resourceEvents", []):
+                            name = i.get("resourceName")
+                            price = i.get("salesInformation", {}).get("price", {}).get("amount")
+                            deposit = i.get("salesInformation", {}).get("deposit", {}).get("amount")
+                        page_list.append({
+                            "Id": event.get('id'),
+                            "Name": name,
+                            "Start Time": f"{date} {month} {year}, {time}",
+                            "End Time": f"{date} {month} {year}, {end_time}",
+                            "Duration": duration,
+                            "Price": price,
+                            "Deposit Amount": deposit,
+                        })
+                    slot_list.append({
+                        "Page":pageNo,
+                        "Slot List":page_list
                     })
-                slot_list.append({
-                    "Page":pageNo,
-                    "Slot List":page_list
-                })
             else:
                 slot_list.append({"error": "Failed to fetch data", "status_code": response.status_code, "Page":pageNo})
             
